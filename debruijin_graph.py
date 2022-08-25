ae9ea6fc9e5cda435de45680a80cadf1129183e4
AaishaM <aaisha@pearlcoral.com>
2022-08-25 16:59:36 +0200

    WIP this solution doesn't even work lol

---
+++ debruijin_graph.py
@@ -0,0 +1,38 @@
+def kmer_generator(k, text):
+    kmers = []
+    for i in range(len(text)-k+1):
+        kmers.append(text[i:i+k])
+    return kmers
+
+def node_generator(k, text):
+    nodes = []
+    for i in range(len(text)-k+1):
+        nodes.append(text[i:i+(k-1)])
+    return nodes
+
+def Overlap(kmers, nodes):
+    adj_list = {}
+    for node in nodes:
+        temp = []
+        for kmer in kmers:
+            if kmer[:-1] == node:
+                temp.append(kmer[1:])
+        adj_list[node] = " ".join(temp)
+    return adj_list
+            
+def OutputFormat(txt):
+    out = []
+    for key, value in txt.items():
+        out.append(f"{str(key)}: {str(value)}")
+    return "\n".join(out)
+
+
+
+data = "AAGATTCTCTAAGA"
+print(OutputFormat(Overlap(kmer_generator(4, data), node_generator(4, data))))
+
+
+# data2 = open("./data/dataset_199_6.txt").read().split()
+
+# with open("./file4.txt", "w") as file:
+#     file.write(OutputFormat(Overlap(kmer_generator(4, data2), node_generator(4, data2))))
